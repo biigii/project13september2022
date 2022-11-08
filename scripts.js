54d19e63606fc4645dbcf7d7ff81a2897712384c
behshad <82580731+biigii@users.noreply.github.com>
2022-11-08 21:01:54 +1300

    javascript

---
+++ scripts.js
@@ -0,0 +1,50 @@
+// alert('hello world')
+
+// console.log('hello world');
+// console.log(1000);
+// console.log(100 + 200);
+
+// const and let
+// var no = 1000;
+// console.log(no)
+
+// var name = 'mary';
+// console.log(name)
+
+// var number1 = 1000
+// var number2 = 2000
+
+// console.log(number1 + number2)
+
+// number1 = 4000
+
+// console.log(number1 + number2)
+
+// kebab -casing  get-element-by-id
+// snake casing  get_element_by_id
+// title casing GetElementById
+// camel casing  <----- this is the one we use for javascript
+// document.getElementById('header').style.color = '#f33';
+// document.getElementById('header').style.border = '2px solid';
+
+// document.getElementById('text').style.backgroundColor = '#000';
+// document.getElementById('text').style.color = '#fff';
+
+// var myBtn = document.getElementById('btn');
+// myBtn.style.backgroundColor = '#fff';
+// myBtn.style.width = '300px';
+// myBtn.style.height = '100px';
+// myBtn.style.fontSize = '2em';
+// myBtn.style.borderRadius = '20px';
+
+// document.querySelector('h1').style.backgroundColor = '#0f0'
+// document.querySelector('#header').style.border = '10px solid'
+// document.querySelector('.heading').style.fontSize = '5em'
+document.querySelector('[data-header]').style.backgroundColor = '#445';
+
+document.querySelector('body').classList.add('homepage');
+document.querySelector('body').classList = 'body';
+
+document.querySelector('h1').classList.remove('heading')
+
+document.body.style.color = '#000';
